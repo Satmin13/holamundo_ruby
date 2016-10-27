27d6816fae9dea19bf8cdf9d7439a78e4b209ecf
Andres Galvez <pocyxDesigner@hotmail.com>
2016-10-27 13:48:22 +0200

    Modificacio Fatima

--- hola.rb
+++ hola.rb
@@ -1,5 +1,7 @@
-puts "Hola como te llamas?"
+﻿puts "Hola como te llamas?"
 nombre = gets.chump
 puts "Hola" + nombre + "!" + "Nice to meet you"
 puts "Aquí estamos prendiendo ruby"
+
+puts "holilla fast"
 
